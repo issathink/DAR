0cde2c1adec2b9d8213399a9dbbe67c0a248bbea
issathink <issamht1@gmail.com>
2016-10-24 18:23:21 +0200

    Trying to compute ...

--- src/tools/Tools.java
+++ src/tools/Tools.java
@@ -1,5 +1,11 @@
 package tools;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -26,6 +32,40 @@ public class Tools {
 	public static String erreurJSON = "{ \"erreur\" : \"Une erreur inattendue s'est produite(JSON). Veuillez verifier et reessayer.\" }";
 	public static String erreurParam = "{ \"erreur\": \"Erreur parametres. Veuillez verifier et reessayer.\" }";
 	public static String ok = "{ \"ok\" : \"ok\" }";
+	public static double LatParisOuest = 48.852159;
+	public static double LonParisOuest = 2.251419;
+	public static double LatParisEst = 48.85206549830757;
+	public static double LonParisEst = 2.425232;
+	public static double LatParisSud = 48.80776790146285;
+	public static double LonParisSud = 2.346954345703125;
+	public static double LatParisNord = 48.90535146044804;
+	public static double LonParisNord = 2.35107421875;
+
+	double haversineKM(double lat1, double long1, double lat2, double long2) {
+		double d2r = Math.PI / 180.0;
+		double dLong = (long2 - long1) * d2r;
+		double dLat = (lat2 - lat1) * d2r;
+		double a = Math.pow(Math.sin(dLat / 2.0), 2) + Math.cos(lat1 * d2r)
+				* Math.cos(lat2 * d2r) * Math.pow(Math.sin(dLong / 2.0), 2);
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		double d = 6367 * c;
+
+		return d;
+	}
+	
+	public static double haversineInKM(double lat1, double long1, double lat2, double long2) {
+		double _eQuatorialEarthRadius = 6378.1370D;
+	    double _d2r = (Math.PI / 180D);
+
+        double dlong = (long2 - long1) * _d2r;
+        double dlat = (lat2 - lat1) * _d2r;
+        double a = Math.pow(Math.sin(dlat / 2D), 2D) + Math.cos(lat1 * _d2r) * Math.cos(lat2 * _d2r)
+                * Math.pow(Math.sin(dlong / 2D), 2D);
+        double c = 2D * Math.atan2(Math.sqrt(a), Math.sqrt(1D - a));
+        double d = _eQuatorialEarthRadius * c;
+
+        return d;
+    }
 
 	public static String getSessionKey() {
 		UUID uid = UUID.randomUUID();
@@ -45,8 +85,8 @@ public class Tools {
 	}
 
 	public static boolean moreThan30Min(long start) {
-		
-		if (start < (getNowMillis() - 30*60*1000))
+
+		if (start < (getNowMillis() - 30 * 60 * 1000))
 			return true;
 		return false;
 	}
@@ -72,8 +112,8 @@ public class Tools {
 			e1.printStackTrace();
 		}
 
-		String query = "select id from " + DBStatic.mysql_db +  ".users where login='"
-				+ login + "'";
+		String query = "select id from " + DBStatic.mysql_db
+				+ ".users where login='" + login + "'";
 
 		try {
 			statement = (Statement) conn.createStatement();
@@ -119,8 +159,8 @@ public class Tools {
 			e1.printStackTrace();
 		}
 
-		String query = "select followed from " + DBStatic.mysql_db +  ".friends where follower='"
-				+ login + "'";
+		String query = "select followed from " + DBStatic.mysql_db
+				+ ".friends where follower='" + login + "'";
 
 		try {
 			statement = (Statement) conn.createStatement();
@@ -164,8 +204,8 @@ public class Tools {
 			e1.printStackTrace();
 		}
 
-		String query = "select date from " + DBStatic.mysql_db +  ".sessions where session_id='"
-				+ key + "'";
+		String query = "select date from " + DBStatic.mysql_db
+				+ ".sessions where session_id='" + key + "'";
 
 		try {
 			statement = (Statement) conn.createStatement();
@@ -195,68 +235,101 @@ public class Tools {
 
 		return date;
 	}
-	
-	public static void removeOldConnexion(String id, String key) throws SQLException{
+
+	public static void removeOldConnexion(String id, String key)
+			throws SQLException {
 		Connection conn = null;
 		Statement statement = null;
-		
+
 		conn = DBStatic.getMySQLConnection();
-		String query = "DELETE FROM " + DBStatic.mysql_db +  ".sessions WHERE user_id='" + Integer.valueOf(id) + "' and session_id!='" + key + "'";
+		String query = "DELETE FROM " + DBStatic.mysql_db
+				+ ".sessions WHERE user_id='" + Integer.valueOf(id)
+				+ "' and session_id!='" + key + "'";
 		statement = (Statement) conn.createStatement();
 		statement.executeUpdate(query);
-		
+
 		try {
 			if (statement != null)
 				statement.close();
 			if (conn != null)
 				conn.close();
-		} catch (SQLException e) {}
+		} catch (SQLException e) {
+		}
 	}
 
 	public static boolean extendSession(String id) {
 		Connection conn = null;
 		Statement statement = null;
-		
+
 		try {
 			conn = DBStatic.getMySQLConnection();
 			statement = (Statement) conn.createStatement();
-			String update = "UPDATE " + DBStatic.mysql_db +  ".sessions SET start = '"
-					+ Tools.getNowMillis()
-					+ "' where session_id = '" + id
-					+ "'";
+			String update = "UPDATE " + DBStatic.mysql_db
+					+ ".sessions SET start = '" + Tools.getNowMillis()
+					+ "' where session_id = '" + id + "'";
 			statement.executeUpdate(update);
 		} catch (SQLException e1) {
 			// e1.printStackTrace();
 			return false;
 		}
-		
+
 		try {
 			if (statement != null)
 				statement.close();
 			if (conn != null)
 				conn.close();
-		} catch (SQLException e) {}
+		} catch (SQLException e) {
+		}
 
 		return true;
 	}
-	
+
 	public static String getUserId(String sessionId, Statement statement) {
 		ResultSet listOfUsers = null;
-		String query = "select user_id, start from " + DBStatic.mysql_db +  ".sessions where session_id='" + sessionId + "'";
-		
+		String query = "select user_id, start from " + DBStatic.mysql_db
+				+ ".sessions where session_id='" + sessionId + "'";
+
 		try {
 			listOfUsers = statement.executeQuery(query);
-			
-			if(listOfUsers.next())
-				if(!Tools.moreThan30Min(Long.parseLong(listOfUsers.getString("start"))))
+
+			if (listOfUsers.next())
+				if (!Tools.moreThan30Min(Long.parseLong(listOfUsers
+						.getString("start"))))
 					return String.valueOf(listOfUsers.getString("user_id"));
 			return null;
 		} catch (SQLException e) {
 			return "Exception: " + e.getMessage();
 		}
 	}
-	
+
 	public static boolean isValidAddress(String adresse) {
+		try {
+			URL url = new URL(adresse);
+			HttpURLConnection connexion = (HttpURLConnection) url
+					.openConnection();
+			connexion.setDoOutput(true);
+			connexion.setDoInput(true);
+			connexion.setRequestProperty("Content-Type", "application/json");
+			connexion.setRequestProperty("Accept", "application/json");
+			connexion.setRequestMethod("GET");
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					(connexion.getInputStream())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+
+			connexion.disconnect();
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// HttpClient httpClient =
 		return true;
 	}
 
