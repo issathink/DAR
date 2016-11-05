e76e0ec0da3d8b398fc22c4394f4c66d989c9daa
faissalCh <fsstach@hotmail.com>
2016-11-05 11:26:20 +0100

    Just rename file

--- src/api/APIs.java
+++ src/api/APIs.java
@@ -5,9 +5,9 @@ import org.json.JSONException;
 
 import tools.Tools;
 
-public class EducationAPI {
+public class APIs {
 
-	public static JSONArray getJSON(double latitude, double longitude, double distance) throws JSONException, Exception {
+	public static JSONArray getEducationJSON(double latitude, double longitude, double distance) throws JSONException, Exception {
 		JSONArray j1 = EtablissementScolairePreBacAPI.getEtablissementScolaireJSON(latitude, longitude, distance);
 		JSONArray j2 = EtablissementScolairePostBacAPI.getEtablissementScolaireJSON(latitude, longitude, distance);
 		return Tools.concatArray(j1, j2);
