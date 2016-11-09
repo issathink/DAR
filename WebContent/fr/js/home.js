7f698872be038f01c01c5f5c418ffb444050ca7d
issathink <issamht1@gmail.com>
2016-11-09 17:37:12 +0100

    doing stuff

--- WebContent/fr/js/home.js
+++ WebContent/fr/js/home.js
@@ -42,7 +42,9 @@ function responseIsConnected(response) {
 		document.getElementById("top_button").innerHTML = "<button onclick='goToChat()' type='button' class='btn btn-default btn-md'>" +
 		"<a class='glyphicon glyphicon-envelope' aria-hidden='true'></a> </button>" +
 		"<button onclick='toInfos()' type='button' class='btn btn-default btn-md'>" +
-		"<a class='glyphicon glyphicon-user' aria-hidden='true'></a> </button>";
+		"<a class='glyphicon glyphicon-user' aria-hidden='true'></a> </button>" +
+		"<button onclick='disconnect();' type='button' class='btn btn-default btn-md'>" +
+		"<a class='glyphicon glyphicon-log-out' aria-hidden='true'></a> </button>";
 
 	} else {
 		document.getElementById("top_button").innerHTML = "<div class='depl_haut'> <a href='signin.html'>Se connecter</a></div>";
@@ -53,6 +55,11 @@ function responseIsConnected(response) {
 function toInfos(){
 	console.log("ToUserProfile");
 	window.location.href = "userProfile.html";
+}
+
+function disconnect() {
+	console.log("disconnect");
+	logout();
 }
 
 function setFieldsToComment(rep){
