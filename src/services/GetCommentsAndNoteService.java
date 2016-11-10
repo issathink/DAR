9887e827f33173d86c77e5a233b6fc69c5c0310d
faissalCh <fsstach@hotmail.com>
2016-11-10 15:48:22 +0100

    Commentaires dans l'ordre

--- src/services/GetCommentsAndNoteService.java
+++ src/services/GetCommentsAndNoteService.java
@@ -4,7 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.lang3.StringEscapeUtils;
@@ -69,15 +76,18 @@ public class GetCommentsAndNoteService {
 						listOfComments = statement.executeQuery();
 						result.put("query", queryGetComments);
 
+
+						List<JSONObject> listJ = new ArrayList<>();
 						while (listOfComments.next()) {
 							String c = listOfComments.getString("comment");
 							JSONObject jObj = new JSONObject();
 
 							String n = listOfComments.getString("note");
 
-							String myDate = listOfComments.getString("date").split(" ")[0];
-							String [] tmpS = myDate.split("-");
-							myDate = tmpS[2]+"/"+tmpS[1]+"/"+tmpS[0];
+							String myDate = listOfComments.getString("date");
+							//							myDate = myDate.split(" ")[0];
+							//							String [] tmpS = myDate.split("-");
+							//							myDate = tmpS[2]+"/"+tmpS[1]+"/"+tmpS[0];
 							if (c != null) {
 								jObj.put("comment", StringEscapeUtils.escapeHtml4(c));
 								jObj.put("adresse", StringEscapeUtils.escapeHtml4(listOfComments.getString("adresse")));
@@ -85,12 +95,41 @@ public class GetCommentsAndNoteService {
 								jObj.put("login", StringEscapeUtils.escapeHtml4(listOfComments.getString("login")));
 								String tmpNote = (n != null) ? n : "";
 								jObj.put("note", tmpNote);
-								result.append("comment", jObj);
+								listJ.add(jObj);
+								//result.append("comment", jObj);
 							}
 							if (n != null) {
 								moy += Double.valueOf(n);
 								cpt++;
 							}
+						}
+
+						Collections.sort(listJ, new Comparator<JSONObject>() {
+							@Override
+							public int compare(JSONObject o1, JSONObject o2) {
+								try {
+									String d1 = o1.getString("date");
+									String d2 = o2.getString("date");
+
+									SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+									Date date1 = sdf.parse(d1);
+									Date date2 = sdf.parse(d2);
+
+									return date2.compareTo(date1);
+								} catch (JSONException e) {
+									e.printStackTrace(); 
+								} catch (ParseException e) {
+									e.printStackTrace();
+								} 				
+								return 0;
+							}
+						});
+						for(JSONObject j : listJ) {
+							String myDate = j.getString("date").split(" ")[0];
+							String [] tmpS = myDate.split("-");
+							myDate = tmpS[2]+"/"+tmpS[1]+"/"+tmpS[0];
+							j.put("date", myDate);
+							result.append("comment", j);
 						}
 
 						result.put("cpt", cpt);
